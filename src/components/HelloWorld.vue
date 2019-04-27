29fc8ed27699fbfacc1721ed593ccd8892d75b35
Ryo Asano <ikarostech@live.jp>
2019-04-28 04:59:04 +0900

    ViewList RawString
    複数ある要素以外のところの表を作った

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,10 +1,25 @@
 <template>
   <div class="data">
 
-    <v-data-table :items="db"></v-data-table>
-      <template slot="item" slot-scope="props">
-        
+    <v-data-table :headers="headers" :items="db">
+      <template slot="items" slot-scope="props">
+        <td>{{ props.item.Number }}</td>
+        <td>{{ props.item.Name }}</td>
+        <td>{{ props.item.Type }}</td>
+        <td>{{ props.item.Hearts }}</td>
+        <td>{{ props.item.Splendor }}</td>
+        <td>{{ props.item.Simple }}</td>
+        <td>{{ props.item.Elegant }}</td>
+        <td>{{ props.item.Active }}</td>
+        <td>{{ props.item.Adult }}</td>
+        <td>{{ props.item.Cute }}</td>
+        <td>{{ props.item.Pure }}</td>
+        <td>{{ props.item.Cool }}</td>
+        <td>{{ props.item.Warm }}</td>
+        <td>{{ props.item.Color }}</td>
+        <td>{{ props.item.Tag }}</td>
       </template>
+    </v-data-table>
   </div>
 </template>
 
@@ -14,7 +29,22 @@ export default {
   name: 'data',
   data: function () {
     return {
-      db: Nikki
+      db: Nikki,
+      headers: [
+        { text: '番号', align: 'center', value: 'Number' },
+        { text: '名前', align: 'center', value: 'Name' },
+        { text: 'タイプ', align: 'center', value: 'Type' },
+        { text: 'レア度', align: 'center', value: 'Hearts' },
+        { text: '華麗', align: 'center', value: 'Splendor' },
+        { text: 'シンプル', align: 'center', value: 'Simple' },
+        { text: 'エレガント', align: 'center', value: 'Elegant' },
+        { text: 'アクティブ', align: 'center', value: 'Active' },
+        { text: '大人', align: 'center', value: 'Adult' },
+        { text: 'キュート', align: 'center', value: 'Cute' },
+        { text: 'ピュア', align: 'center', value: 'Pure' },
+        { text: 'クール', align: 'center', value: 'Cool' },
+        { text: 'ウォーム', align: 'center', value: 'Warm' },
+      ]
     }
   }
 }
